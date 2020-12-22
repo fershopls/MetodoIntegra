741b018d6b525d563bbe47ec3345e8d3ca928d4e
Fersho Pls <shaaf4ever@gmail.com>
2020-12-22 15:46:00 -0600

    list created

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -2,27 +2,48 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Habitaciones</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Habitaciones</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+
+      <!-- Item as a Button -->
+      <div v-for="room in rooms" v-bind:key="room.id">
+      <ion-item button href="/room">
+        <ion-label>
+          {{ room.name }}
+        </ion-label>
+      </ion-item>
+      </div>
+
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+//import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default  {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  data() {
+    return {
+      buttonClick () {
+        console.log("XD")
+      },
+      rooms : [
+        {id:1, name: "Love Laguna"},
+        {id:2, name: "Jardin"},
+        {id:3, name: "Residencia"},
+        {id:4, name: "Family Suite"},
+      ]
+    }
+  }
 }
 </script>
