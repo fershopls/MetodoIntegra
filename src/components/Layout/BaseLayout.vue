aec44824a80df894f1e95e09b9f09ae3fe2e2c67
Fersho Pls <shaaf4ever@gmail.com>
2020-12-22 23:28:30 -0600

    BaseLayout added

---
+++ src/components/Layout/BaseLayout.vue
@@ -0,0 +1,37 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-back-button slot="start" default-href="/"></ion-back-button>
+                <ion-title>{{ pageTitle }}</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content>
+            <slot />
+        </ion-content>
+    </ion-page>
+</template>
+
+
+<script>
+import { 
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonBackButton,
+} from "@ionic/vue";
+
+export default {
+    props: ["pageTitle"],
+    components: {
+        IonPage,
+        IonHeader,
+        IonToolbar,
+        IonTitle,
+        IonContent,
+        IonBackButton,
+    }
+}
+</script>
