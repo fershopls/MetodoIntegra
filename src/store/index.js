de1a202a2526c652aef665337a88410a62c0b580
Fersho Pls <shaaf4ever@gmail.com>
2020-12-23 01:15:37 -0600

    Adjustments to store

--- src/store/index.js
+++ src/store/index.js
@@ -6,16 +6,35 @@ const store = createStore({
             protocol_list: []
         }
     },
+    mutations: {
+        createProtocol(state, data) {
+        const newProtocol = {
+          id: new Date().getTime(),//new Date().toISOString(),
+          name: data.name,
+          beliefs: [],
+        };
+  
+        state.protocol_list.push(newProtocol);
+      }
+    },
+    actions: {
+      createProtocol(context, data) {
+        context.commit('createProtocol', data);
+      }
+    },
     getters: {
         allProtocols(state) {
             return state.protocol_list
         },
         
-        protocolById(state){
+        
+        getProtocolById(state) {
             return (protocolId) => {
-                return state.protocol_list.find((protocol) => protocol.id === protocolId);
+                return state.protocol_list.find((protocol) => protocol.id == protocolId);
             };
         },
+
+
     },
 });
 
