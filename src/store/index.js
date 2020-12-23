2a0b3f2b33c188e97a59a66c1d29581217ab744f
Fersho Pls <shaaf4ever@gmail.com>
2020-12-22 23:53:40 -0600

    added store

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+    state() {
+        return {
+            protocol_list: []
+        }
+    },
+    getters: {
+        allProtocols(state) {
+            return state.protocol_list
+        },
+        
+        protocolById(state){
+            return (protocolId) => {
+                return state.protocol_list.find((protocol) => protocol.id === protocolId);
+            };
+        },
+    },
+});
+
+export default store;
