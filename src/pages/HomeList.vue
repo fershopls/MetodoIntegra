1658cdbf027866ccbe84793ce73a0d791a6d70d3
Fersho Pls <shaaf4ever@gmail.com>
2020-12-22 19:58:08 -0600

    Home List created

---
+++ src/pages/HomeList.vue
@@ -0,0 +1,61 @@
+<template>
+    <base-layout pageTitle="Mis Protocolos">
+        
+        
+        <ion-list>
+            <ion-item v-for="protocol in list" button :router-link="'/protocol/'+protocol.id" v-bind:key="protocol.id">
+                <ion-label>{{ protocol.name }}</ion-label>
+                <ion-note slot="end">{{ protocol.date }}</ion-note>
+            </ion-item>
+        </ion-list>
+
+        
+        <!-- fab placed to the (vertical) center and end -->
+        <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+            <ion-fab-button>
+                <ion-icon src="/assets/add.svg" />
+            </ion-fab-button>
+        </ion-fab>
+
+    </base-layout>
+</template>
+
+<script>
+import BaseLayout from "@/components/Layout/BaseLayout.vue"
+import {
+    IonList,
+    IonItem,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonLabel,
+    IonNote,
+} from "@ionic/vue"
+import add from "ionicons"
+
+export default {
+    components: {
+        IonList,
+        IonItem,
+        IonFab,
+        IonFabButton,
+        IonIcon,
+        IonLabel,
+        IonNote,
+    },
+    data(){
+        return {
+            list: [
+                {id: 1, name: "Mi protocolo 1", date: "Dic 28"},
+                {id: 2, name: "Mi proto 22222", date: "Dic 20"},
+            ]
+        }
+    },
+    setup(){
+        return {
+            add
+        }
+    }
+    
+}
+</script>
