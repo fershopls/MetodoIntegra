aa60c32a71efb913d0d907ed9ed814673e16b7e3
Fersho Pls <shaaf4ever@gmail.com>
2020-12-27 11:52:07 -0600

    Popover designed with working icons

--- src/pages/ProtocolOverview/Popover.vue
+++ src/pages/ProtocolOverview/Popover.vue
@@ -3,9 +3,16 @@
         <ion-list-header>
             <strong>Opciones</strong>
         </ion-list-header>
-        <ion-item button>Exportar Protocolo</ion-item>
-        <ion-item button>Cambiar Nombre</ion-item>
-        <ion-item lines="none" button>Eliminar Protocolo</ion-item>
+        <ion-item button>
+            <ion-icon :icon="shareOutline" slot="end"/>
+            Exportar Protocolo</ion-item>
+        <ion-item button>
+            <ion-icon :icon="pencilOutline" slot="end"/>
+            Cambiar Nombre</ion-item>
+        <ion-item lines="none" button>
+            <ion-icon :icon="trashOutline" slot="end"/>
+            Eliminar Protocolo
+            </ion-item>
         <!-- <ion-item lines="none" detail="false" button>Cerrar</ion-item> -->
     </ion-list>
 </template>
@@ -16,13 +23,29 @@ import {
     IonItem,
     IonListHeader,
     IonList,
+    IonIcon,
 } from '@ionic/vue';
+
+import {
+    shareOutline,
+    pencilOutline,
+    trashOutline,
+} from "ionicons/icons"
 
 export default {
   components: {
       IonItem,
       IonListHeader,
-      IonList
+      IonList,
+      IonIcon,
   },
+  setup(){
+      return {
+        shareOutline,
+        pencilOutline,
+        trashOutline,
+
+      }
+  }
 };
 </script>
