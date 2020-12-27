17cfa1f02186c7c364ac9629a7a6a81e59f5a8fd
Fersho Pls <shaaf4ever@gmail.com>
2020-12-26 18:15:29 -0600

    Test modal

---
+++ src/pages/test/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <ion-page>
+    <ion-content class="ion-padding">
+      <ion-button @click="openModal">Open Modal</ion-button>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonButton, IonContent, IonPage, modalController } from '@ionic/vue';
+import Modal from '@/components/WelcomeScreen.vue'
+
+export default {
+  components: { IonButton, IonContent, IonPage },
+  methods: {
+    async openModal() {
+      const modal = await modalController
+        .create({
+          component: Modal,
+          cssClass: 'my-custom-class',
+          componentProps: {
+            title: 'New Title'
+          },
+        })
+      return modal.present();
+    },
+  },
+}
+</script>
